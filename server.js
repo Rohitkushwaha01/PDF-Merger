a8023af56419912b5456d4b2972175cc76c900a4
Rohit Kushwaha <rohitkushwahac@gmail.com>
2022-11-16 10:11:08 +0530

    PDF merger

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const multer = require('multer');
 const upload = multer({dest: 'uploads/'});
 const path = require('path');
+const mergepdfs = require('./merge');
 const app = express();
 port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, "templates/index.html"));
 })
 
-app.post('/merge', upload.array('pdfs', 10), function(req, res, next){
-    console.log(req.files);
-    res.send({"data": req.files});
+app.post('/merge', upload.array('pdfs', 2), async function(req, res, next){
+    // console.log(req.files);
+    // res.send({data: req.files});
+    await mergepdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path));
+    res.redirect("http://localhost:3000/merge/merged.pdf");
 })
 
 app.listen(port, ()=>{
